17d10d88b365d298223af2fcbb61b500aea5323a
stephaneghozzi <hello@stephaneghozzi.com>
2025-07-19 17:54:30 +0200

    refactoring of data loader

--- src/data/catalogues.json.js
+++ src/data/catalogues.json.js
@@ -10,11 +10,21 @@ async function json(url) {
     return await response.json();
 }
 
-const branches_gh = await json("https://api.github.com/repos/stephaneghozzi/pgs-ds-cat-data/branches");
+const repo = "pgs-ds-cat-data";
+const owner = "stephaneghozzi";
+const cat_file = "catalogue.json";
+
+const branches_gh = await json(
+  `https://api.github.com/repos/${owner}/${repo}/branches`
+);
 const branches = branches_gh.map(d => d.name);
 const branches_ok = [];
 for (let i = 0; i < branches.length; i++) {
-  branches_ok.push(await json_ok(`https://raw.githubusercontent.com/stephaneghozzi/pgs-ds-cat-data/refs/heads/${branches[i]}/catalogue.json`))
+  branches_ok.push(
+    await json_ok(
+      `https://raw.githubusercontent.com/${owner}/${repo}/refs/heads/${branches[i]}/${cat_file}`
+    )
+  )
 }
 
 const catalogue = [];
@@ -23,7 +33,9 @@ for (let i = 0; i < branches.length; i++) {
   if (branches_ok[i]) {
     catalogue[j] = {
       branch: branches[i],
-      catalogue: await json(`https://raw.githubusercontent.com/stephaneghozzi/pgs-ds-cat-data/refs/heads/${branches[i]}/catalogue.json`)
+      catalogue: await json(
+        `https://raw.githubusercontent.com/${owner}/${repo}/refs/heads/${branches[i]}/${cat_file}`
+      )
     };
     j++;
   }
